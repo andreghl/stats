8225167049880c4ba78490f9f531f3d6e0abed05
PotatoSauce <andrelghonda@gmail.com>
2024-08-17 20:13:22 +0200

    Updates 2024-08-17

--- lm/lm.jl
+++ lm/lm.jl
@@ -1,8 +1,6 @@
 using DataFrames, Distributions, LinearAlgebra
 
-
-
-function pt(T, df = 300) 
+function pt(T, df = 100) 
 
    Dist = TDist(df)
    Pvalue = 1 - cdf(Dist, T)
@@ -10,43 +8,22 @@ function pt(T, df = 300)
    return Pvalue
 end
 
-function lm(y, X)
+function lm(y::Vector{Float64}, X::Matrix{Float64})
 
-    n = size(X, 1) # Sample size
-    K = size(X, 2) # Number of regressors
-    
-    y = values(y)
-    i = ones(n)
-    X = [i X]
-    X = values(X)
+    beta = inv(X'X)X'y
 
-    beta = inv(X'X)X'y # Estimated coefficients
-    yHat = X * beta # Estimates for dependent
-    e = y - (X * beta) # Residuals
+    n = length(y)
+    k = size(X, 2)
+    e = y - (X * beta)
 
-    SSR = e'e
-    s2 = SSR / (n - K)
-    s = sqrt(s2)
-
-    i = ones(n)
-    yMean = inv(i'i)i'y
-    T = (y .- yMean)
-    TSS = T'T
-
-    RSq = 1 - (SSR / TSS)
-    Adj = 1 - (SSR / (n - K)) / (TSS / (n - 1))
-
+    s2 = e'e / (n - k)
     betaSE = sqrt.(diag(inv(X'X) * s2))
+    tstat = beta ./ betaSE
+    pvalue = [pt(t, n - 1) for t in tstat]
 
-    Tvalue = beta ./ betaSE
-    df = n - 1
-    Pvalue = [pt(t, df) for t in Tvalue]
-
-    Lower = beta .- 1.96 .* betaSE
-    Upper = beta .+ 1.96 .* betaSE
-
-    output = DataFrame(beta = beta, StdErr = betaSE, Tvalue = Tvalue, 
-    Pvalue = Pvalue, LowerBound = Lower, UpperBound = Upper)
-
-    return (output)
+    return DataFrame(beta = beta, betaSE = betaSE, tstat = tstat, 
+    pvalue = pvalue)
 end
+
+
+
